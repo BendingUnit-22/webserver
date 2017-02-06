f83f383d7982bbac3a52a16195c260fa5e45ba59
Rixing <wur1@wit.edu>
2017-02-06 09:00:47 -0500

    d

--- src/components/Visualization/D3Components/Ideology/D3Node.js
+++ src/components/Visualization/D3Components/Ideology/D3Node.js
@@ -5,40 +5,57 @@
 import * as d3 from 'd3';
 
 
+    const Chart = (dom, info, styles) => {
 
+        const dx = styles.dx,
+              dy = styles.dy,
+              width = styles.width,
+              height = styles.height;
+
+        const grouplabel = info[0],
+                data = info[1];
+
+
+
+        const svg =
+            d3.select(dom)
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height);
+
+
+        const group =
+            svg.append('g')
+            .attr('transform', `translate(${dx},${dy})`);
+
+
+        d3.select(dom).attr('class', 'node');
+
+        group.append('text')
+             .text(grouplabel)
+            .attr('x', 0)
+            .attr('y', 0);
+
+        return dom;
+
+    };
 
 
     export default (props) => {
+            const node = document.createElement('div');
 
-        const categories = props.cats;
+            const cat1 = props.cats[0];
 
+            Chart(node, cat1, {
+               dx: 0,
+                dy: 0,
+                width: "100%",
+                height: 200
+            });
 
-        const node = document.createElement('div');
-
-        const BoxH = 200;
-
-        const svg = d3.select(node)
-                    .append('svg')
-                    .attr('width', props.width)
-                    .attr('height', props.height);
-
-
-        const groups =  svg.selectAll('g')
-                .data(categories);
-
-             groups.enter()
-                    .append('g')
-                    .each(function (segment, i) {
-
-                        d3.select(this)
-                            .append('p')
-
-                            .text(segment[0]);
-
-                    });
-
-        return node;
+        // Test
+        //
+         return node;
     };
-
 
 
