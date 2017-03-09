151dbd4d4a3255466b6bc51ec63275e75f97fbc3
BendingUnit-22 <wur1@wit.edu>
2017-03-09 02:17:08 -0500

    added geo orthographic;

--- public/geomapping/index.js
+++ public/geomapping/index.js
@@ -3,34 +3,68 @@
 
 var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 var height = window.innerHeight || document.documentElement.clientHeight|| document.body.clientHeight;
+// D3
+width -= 100;
+height -= 100;
+var projection = d3.geoOrthographic();
+
+var path = d3.geoPath()
+        .projection(projection);
 
 var svg = d3.select('body')
+            .append('div')
+            .attr('class', 'wrapper')
             .append('svg')
             .attr('width', width)
             .attr('height', height);
 
-var projection = d3.geoOrthographic().translate([width/2, height/2]);
+var graticule = d3.geoGraticule();
+svg.append('path').datum(graticule).attr("class", "graticule").attr('d', path);
 
-var path = d3.geoPath().projection(projection);
-
-queue()
-.defer(d3.json, 'world.geo.json')
-.await(ready);
+queue().defer(d3.json, 'world.geo.json').await(ready);
 
 function ready(err, world) {
       if (err) throw err;
-
           svg.selectAll('path')
              .data(world.features)
              .enter()
              .append('path')
              .attr('d', path)
-             .on('click', rotateTransition);
+             .attr('class', 'clickable')
+             .attr('id', function(d) {return d.id;})
+             .on('click', onClicked);
+
+
+      //
+      // setInterval(function(){
+      //
+      //
+      //   var randomIndex =  Math.floor(Math.random() * world.features.length);
+      //     onClicked(world.features[randomIndex]);
+      //
+      // }, 3000);
 }
 
 
-function rotateTransition(country){
-      var p = d3.geoCentroid(country);
-      var rotation = d3.interpolate(projection.rotate(),[-p[0], -p[1]]);
-      console.log(roate);
+
+function onClicked(country){
+          // Reset
+          d3.selectAll(".clicked")
+             .classed("clicked", false);
+         // mark selected
+         d3.select('#' + country.id).classed('clicked', true);
+         // animation
+         (function transition(){
+           d3.select('.clicked').transition()
+                     .duration(1250)
+                     .tween('rotate', function(){
+                       var p = d3.geoCentroid(country);
+                       var r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+                       return function(t){
+                         projection.rotate(r(t));
+                         svg.selectAll("path").attr("d", path);
+                       };
+               });
+         })();
+         svg.append('path').datum(graticule).attr("class", "graticule").attr('d', path);
 }
